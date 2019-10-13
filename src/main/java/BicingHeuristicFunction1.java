40900c6b23e1f2c38e0ca56bb32ca033da58f9d1
Luis Oriol Soler Cruz <luisoriol.solercruz@gmail.com>
2019-10-13 18:56:22 +0200

    Add 'HeuristicFunction1'

--- src/main/java/BicingHeuristicFunction1.java
+++ src/main/java/BicingHeuristicFunction1.java
@@ -1,2 +1,10 @@
-public class BicingHeuristicFunction1 {
+import aima.search.framework.HeuristicFunction;
+
+// Tiene en cuenta MAXIMIZAR los beneficios obtenidos al trasladar bicis
+public class BicingHeuristicFunction1 implements HeuristicFunction {
+    @Override
+    public double getHeuristicValue(Object state) {
+        BicingSolution solution = (BicingSolution) state;
+        return solution.getBeneficios();
+    }
 }
